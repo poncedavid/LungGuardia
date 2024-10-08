abaaee14f43d0c7f4b78f8c525382a8617abb574
poncedavid <da.ponce@duocuc.cl>
2024-10-07 22:01:55 -0300

    Agregando configuración de Tailwind CSS, PrimeNG y estilos globales.

--- lung-guardia/src/app/app.component.ts
+++ lung-guardia/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,        // Módulo de botón de PrimeNG
+    InputTextModule,     // Módulo de input de texto de PrimeNG
+    // Otros módulos aquí
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
